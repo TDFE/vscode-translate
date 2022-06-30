c127ba06d8024d11e83d18cba32b42ac61b4e427
郑泳健 <yongjian.zheng@tongdun.net>
2022-06-30 10:35:14 +0800

    从require改为import

--- src/extension.ts
+++ src/extension.ts
@@ -1,118 +1,74 @@
 import * as vscode from 'vscode';
 import { getLanguageMap, getCurrentLanguage, setLineDecorations } from './utils/index';
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
     // @ts-ignore
-    const kiwiPath = vscode.workspace.workspaceFolders[0].uri.path + '/.kiwi';
+    const octopusPath = vscode.workspace.workspaceFolders[0].uri.path + '/.octopus';
     // 获取当前工程的所有语言包,放到globalState
-    context.globalState.update('languageMap', getLanguageMap(kiwiPath));
+    context.globalState.update('languageMap', await getLanguageMap(octopusPath, context));
     // 获取当前的语言
     context.globalState.update('currentLanguage', getCurrentLanguage());
 
     let activeEditor = vscode.window.activeTextEditor;
 
     if (activeEditor) {
-        setLineDecorations(context, activeEditor)
+        setLineDecorations(context, activeEditor);
     }
 
     // 切换文档时候重新获取当前文档中的中文文案
     const changeActiveTextEditor = vscode.window.onDidChangeActiveTextEditor(editor => {
         activeEditor = editor;
         if (editor) {
-            setLineDecorations(context, editor)
+            setLineDecorations(context, editor);
         }
-    })
+    });
 
-    // 当文档内容发生变化时候重新检测文档中的中文文案
-    const changeTextDocument = vscode.workspace.onDidChangeTextDocument((evnet) => {
-        if (activeEditor && evnet.document === activeEditor.document) {
-            setLineDecorations(context, activeEditor)
-        }
-    })
+    // // 当文档内容发生变化时候重新检测文档中的中文文案
+    // const changeTextDocument = vscode.workspace.onDidChangeTextDocument((evnet) => {
+    // if (activeEditor && evnet.document === activeEditor.document) {
+    //         setLineDecorations(context, activeEditor);
+    //     }
+    // });
 
     // 监听设置里面的配置是否有更新
-    vscode.workspace.onDidChangeConfiguration(function (event) {
+    vscode.workspace.onDidChangeConfiguration(async function (event) {
         const configList = ['tongdun'];
         const affected = configList.some(item => event.affectsConfiguration(item));
         if (affected) {
             context.globalState.update('currentLanguage', getCurrentLanguage());
+            context.globalState.update('languageMap', await getLanguageMap(octopusPath, context));
+            if (activeEditor) {
+                setLineDecorations(context, activeEditor);
+            }
         }
     });
 
-    // 监听.wiki文件夹的内容更改
-    const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(kiwiPath, '**/*.js'), false, false, false);
+    // 监听.octopus文件夹的内容更改
+    const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(octopusPath, '**/*.js'), false, false, false);
 
-    watcher.onDidChange(e => { // 文件发生更新
-        context.globalState.update('languageMap', getLanguageMap(kiwiPath));
+    watcher.onDidChange(async e => { // 文件发生更新
+        context.globalState.update('languageMap', await getLanguageMap(octopusPath, context));
         if (activeEditor) {
-            setLineDecorations(context, activeEditor)
+            setLineDecorations(context, activeEditor);
         }
     });
 
-    watcher.onDidCreate(e => { // 新建了js文件
-        context.globalState.update('languageMap', getLanguageMap(kiwiPath));
+    watcher.onDidCreate(async e => { // 新建了js文件
+        context.globalState.update('languageMap', await getLanguageMap(octopusPath, context));
         if (activeEditor) {
-            setLineDecorations(context, activeEditor)
+            setLineDecorations(context, activeEditor);
         }
     });
 
-    watcher.onDidDelete(e => { // 删除了js文件
-        context.globalState.update('languageMap', getLanguageMap(kiwiPath));
+    watcher.onDidDelete(async e => { // 删除了js文件
+        context.globalState.update('languageMap', await getLanguageMap(octopusPath, context));
         if (activeEditor) {
-            setLineDecorations(context, activeEditor)
+            setLineDecorations(context, activeEditor);
         }
     });
 
     context.subscriptions.push(changeActiveTextEditor);
-    context.subscriptions.push(changeTextDocument);
-
-    // 这是悬浮时候显示文本内容
-    // const disposable = vscode.languages.registerHoverProvider(['json', 'javascript', 'typescript'], {
-    //     provideHover(document, position) {
-    //         const fileName = document.fileName;
-
-    //         // 获取一整行的内容
-    //         const lineText = document.getText(new vscode.Range(
-    //             position.line,
-    //             0,
-    //             position.line,
-    //             500
-    //         ));
-
-    //         // 如果是.kiwi文件夹或者这一行没有I18N这个字符就没有提示
-    //         if (fileName.includes('.kiwi') || !lineText.includes('I18N')) {
-    //             return undefined;
-    //         }
-
-    //         let paths: string[] = [];
-
-    //         const currentLanguage = context.globalState.get('currentLanguage');
-
-    //         if (!currentLanguage) {
-    //             return new vscode.Hover('请先在vscode设置里面配置语言');
-    //         }
-
-    //         try {
-    //             // @ts-ignore
-    //             const matchPath = lineText.match(/I18N[a-zA-Z.0-9]*/g) ? lineText.match(/I18N[a-zA-Z.0-9]*/g)[0] : '';
-    //             paths = matchPath.split('.');
-    //             paths.shift();
-    //         } catch (e) {
-    //             paths = [];
-    //         }
-
-    //         // 获取当前的语言包
-    //         const languageMap = context.globalState.get('languageMap');
-
-    //         if (languageMap) {
-    //             // @ts-ignore
-    //             const value = getValue(languageMap[currentLanguage], paths);
-    //             return new vscode.Hover(value ? String(value) : `当前语言为${currentLanguage},匹配不到对应的key`);
-    //         }
-
-    //         return undefined;
-    //     }
-    // });
+    // context.subscriptions.push(changeTextDocument);
 }
 
 export function deactivate(context: vscode.ExtensionContext) {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,11 +3,19 @@
  * @Author: 郑泳健
  * @Date: 2022-05-27 18:22:28
  * @LastEditors: 郑泳健
- * @LastEditTime: 2022-06-07 19:15:34
+ * @LastEditTime: 2022-06-30 10:18:46
  */
+require('ts-node').register({
+    compilerOptions: {
+        module: 'commonjs'
+    }
+});
+
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as _ from 'lodash';
+import * as shell from 'shelljs';
+import * as path from 'path';
 
 interface Language {
     language: string;
@@ -30,22 +38,29 @@ class Position {
  * 获取所有语言包的配置
  * @returns
  */
-export const getLanguageMap = (kiwiPath: string) => {
+export const getLanguageMap = async (octopusPath: string, context: vscode.ExtensionContext) => {
+    const lang: string = context.globalState.get('currentLanguage') || 'zh-CN';
     try {
-        const files = fs.readdirSync(kiwiPath);
+        await shell.rm('-rf', path.resolve(__dirname, '../temp'));
+        await shell.cp(
+            '-R',
+            `${octopusPath}/${lang}/`,
+            path.resolve(__dirname, '../temp')
+        );
 
-        return files.reduce((total, item) => {
-            // 因为require会缓存结果，导致每次结果都不更新，所以需要清除
-            Object.keys(require.cache).forEach(function (key) {
-                delete require.cache[key];
-            });
-            // @ts-ignore
-            total[item] = require(`${kiwiPath}/${item}`);
-            return total;
-        }, {});
+        const filelist = getNeedChangeNameFileList(path.resolve(__dirname, '../temp'), '.js');
+
+        await changeFileSuffix(filelist, '.js', '.ts');
+        Object.keys(require.cache).forEach(function (key) {
+            delete require.cache[key];
+        });
+        const { default: langValue } = require(path.resolve(__dirname, '../temp/index.ts'));
+        return {
+            [lang]: langValue
+        };
     } catch (e: any) {
         vscode.window.showErrorMessage(`获取配置错误,错误具体信息: ${e.message}`);
-        return {}
+        return {};
     }
 };
 
@@ -112,7 +127,7 @@ export const transformPosition = (pos: Position, editorText: string, toLastCol?:
         last = new vscode.Position(line, ch + width);
     }
     return new vscode.Range(first, last);
-}
+};
 
 /**
  * 设置行内提示
@@ -137,12 +152,12 @@ export const setLineDecorations = (context: vscode.ExtensionContext, activeEdito
                     textDecoration: 'none;'
                 }
             } as vscode.DecorationInstanceRenderOptions
-        }
+        };
     });
 
     // @ts-ignore
     activeEditor.setDecorations(annotationDecoration, decorations);
-}
+};
 
 /**
  * 查找 I18N 表达式
@@ -154,7 +169,7 @@ export const findI18NPositions = (context: vscode.ExtensionContext, code: string
     // @ts-ignore
     const I18N = languageMap[currentLanguage];
     if (!I18N) {
-        return []
+        return [];
     }
 
     const positions = [] as Position[];
@@ -167,7 +182,7 @@ export const findI18NPositions = (context: vscode.ExtensionContext, code: string
     });
 
     return matchPositions;
-}
+};
 
 /** 使用正则匹配{{}} */
 const getRegexMatches = (I18N: string, code: string) => {
@@ -203,7 +218,7 @@ const getRegexMatches = (I18N: string, code: string) => {
         }
     });
     return positions;
-}
+};
 
 // /**
 //  * 获取到翻译的结果
@@ -225,3 +240,41 @@ const getRegexMatches = (I18N: string, code: string) => {
 //         return '';
 //     }
 // };
+
+/**
+ * 动态修改文件名
+ * @param {*} filelist 需要修改后缀的文件列表, 每一项都不带后缀 string[]
+ * @param {*} originSuffix 原后缀
+ * @param {*} changedSuffix 新后缀
+ */
+const changeFileSuffix = (filelist: string[], originSuffix: string, changedSuffix: string) => {
+    return Promise.all(filelist.map((fileName) => {
+        console.log(fileName);
+        fs.renameSync(fileName + originSuffix, fileName + changedSuffix);
+    }));
+};
+
+/**
+ * 递归获取所有要修改名字的目录
+ * @param {*} path 要翻译的目录
+ * @param {*} originSuffix 要翻译的文件原后缀
+ * @param {*} fileList 返回哪些文件要修改后缀
+ * @returns
+ */
+const getNeedChangeNameFileList = (path: string, originSuffix: string, fileList: string[] = []) => {
+    const files = fs.readdirSync(path);
+
+    files.forEach(function (file) {
+        const stat = fs.statSync(path + '/' + file);
+        if (stat.isDirectory()) {
+            getNeedChangeNameFileList(path + '/' + file, originSuffix, fileList);
+        }
+        if (stat.isFile() && file.endsWith(originSuffix)) {
+            // 去掉文件后缀，因为后面还要转回来
+            const filename = file.substring(0, file.lastIndexOf('.'));
+            fileList.push(`${path}/${filename}`);
+        }
+    });
+
+    return fileList;
+};
